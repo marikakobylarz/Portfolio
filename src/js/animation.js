55b05c73e65b8f52116eca7bf328167b428872f4
Marika Kobylarz <akiramoo7@gmail.com>
2020-02-11 19:47:41 +0100

    add new project, change styles

--- src/js/animation.js
+++ src/js/animation.js
@@ -4,36 +4,26 @@ const skills = document.querySelector(".skills");
 const skill = document.querySelectorAll(".skills__item .fab");
 const windowHeight = window.innerHeight;
 
+document.addEventListener("scroll", function() {
+  let scrollPosition = window.scrollY;
 
-document.addEventListener("scroll", function () {
-
-    let scrollPosition = window.scrollY;
-
-    if (scrollPosition > (projects.offsetTop - windowHeight)) {
-        project.forEach((pro, index) => {
-            setTimeout(() => {
-                pro.style.transform = "translateX(0)";
-
-            }, 1000 * index);
-        });
-    };
+  if (scrollPosition > projects.offsetTop - windowHeight) {
+    project.forEach((pro, index) => {
+      setTimeout(() => {
+        pro.style.transform = "translateX(0)";
+      }, 1000 * index);
+    });
+  }
 });
 
-document.addEventListener("scroll", function () {
+document.addEventListener("scroll", function() {
+  let scrollPosition = window.scrollY;
 
-    let scrollPosition = window.scrollY;
-
-    if (scrollPosition > (skills.offsetTop - windowHeight)) {
-
-        skill.forEach((icon, index) => {
-            setTimeout(() => {
-                console.log("jest");
-                icon.style.transform = "translateX(0) rotate(360deg)";
-
-            }, 300 * index);
-        });
-    };
+  if (scrollPosition > skills.offsetTop - windowHeight) {
+    skill.forEach((icon, index) => {
+      setTimeout(() => {
+        icon.style.transform = "translateX(0) rotate(360deg)";
+      }, 300 * index);
+    });
+  }
 });
-
-
-
--- src/js/projects.js
+++ src/js/projects.js
@@ -1,4 +1,3 @@
-
 const projectsList = document.querySelector(".projects__list");
 
 const projects = [
@@ -11,20 +10,28 @@ const projects = [
     code: "https://github.com/marikakobylarz/Portfolio"
   },
   {
-    name: "ReefTime - Marine Tanks",
+    name: "My Music App",
     description:
-      "Build with Create React App. SPA using React Router. Deployed with Firebase.",
-    img: '../assets/page1.png',
-    site: "https://reef-time-marine-tanks.web.app/",
-    code: "https://github.com/marikakobylarz/ReefTime-MarineTanks"
+      "Aplication build with Create-React-App for searching and streaming videos from YouTube. Deployed with now.sh.",
+    img: "../assets/mymusicapp.png",
+    site: "https://mymusicapp.now.sh/",
+    code: "https://github.com/marikakobylarz/mymusicapp"
   },
   {
     name: "Weather app",
     description:
       "Simple weather application build with Create React App. Data is being downloaded from http://api.openweathermap.org/. Deployed with Firebase.",
-    img: '../assets/weatherapp.png',
+    img: "../assets/weatherapp.png",
     site: "https://weather-app-d0bfd.web.app/",
     code: "https://github.com/marikakobylarz/WeatherApp"
+  },
+  {
+    name: "ReefTime - Marine Tanks",
+    description:
+      "Build with Create React App. SPA using React Router. Deployed with Firebase.",
+    img: "../assets/page1.png",
+    site: "https://reef-time-marine-tanks.web.app/",
+    code: "https://github.com/marikakobylarz/ReefTime-MarineTanks"
   }
 ];
 
@@ -35,10 +42,9 @@ projects.forEach(project => {
   <div class="projects__header">
     <h4 class="projects__name">${project.name}</h4>
     </div>
-    <p class="projects__text">${project.description}</p>
-      <div class="projects__image" style="background-image:url(${
-    project.img
-    })">  <div class="projects__links">        
+    <div class="projects__text"><p class="projects__description">${project.description}</p></div>
+    <div class="projects__image" style="background-image:url(${project.img})">
+    <div class="projects__links">        
     <p class="projects__site">
       <a href= ${project.site} class="projects__link">
       <i class="projects__icon fas fa-desktop style"></i></a></p>                                           
